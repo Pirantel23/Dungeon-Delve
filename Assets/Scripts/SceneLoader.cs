94e7c1f3536aaefcecd21e57f1df45a066548169
Pirantel23 <max420652@gmail.com>
2023-04-27 23:50:34 +0500

    Scene loader added

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    [SerializeField] private Image loadingScreen;
+    public void LoadScene(int sceneIndex)
     {
-        Debug.Log($"Loading scene {sceneName}");
-        SceneManager.LoadScene(sceneName);
+        Debug.Log($"Loading scene {sceneIndex}");
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    private IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        if (loadingScreen is null) yield break;
+        var operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            loadingScreen.color = new Color(255, 255, 255, operation.progress);
+            yield return null;
+        }
     }
 }
